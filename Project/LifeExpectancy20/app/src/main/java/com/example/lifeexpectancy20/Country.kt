b69dcfc6f565aa334f3c7bd058ca4d9ec6a31632
vandyG <vandy.goel23@gmail.com>
2019-12-15 19:38:15 +0530

    backup dec 2019

--- Project/LifeExpectancy20/app/src/main/java/com/example/lifeexpectancy20/Country.kt
+++
@@ -1,8 +0,0 @@
-package com.example.lifeexpectancy20
-
-class Country(
-    var countryName: String = "",
-    var serialNumber: Long = -1,
-    var currLifeExp: Double = Double.NaN,
-    var countryCode: String = ""
-)
